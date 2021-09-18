a6bf54358ebaf2607ef3ea5f42ff42a9391c5ba4
Anton Baric <anton.baric99@gmail.com>
2021-09-19 01:49:08 +0200

    functioning fetch search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,30 +2,38 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function SearchBar() {
+	const [songs, setSongs] = useState([])
+    const {content = [] } = songs;
+	const [searchString, setSearchString] = useState()
+	const [searchStringFromButtonClick, setSearchStringFromButtonClick] = useState()
 
-    const [songs, setSongs] = useState([])
+	useEffect(() => {
+		axios
+			.get(`https://yt-music-api.herokuapp.com/api/yt/songs/${searchString}`)
+			.then(res => {
+        console.log(res)
+        setSongs(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [searchStringFromButtonClick])
 
-    useEffect(() => {
-        axios.get('https://yt-music-api.herokuapp.com/api/yt/songs/luvdeluxe')
-        .then(res => {
-            console.log(res);
-            setSongs(res.data)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
+	const handleClick = () => {
+		setSearchStringFromButtonClick(searchString)
+	}
 
-    return <>
-    <input type="text" />
-    <div>
-        <ul>
-            {songs.map(song => (
-                <li key={song.videoId}>{song.content}</li> )
-                )}
-        </ul>
-    </div>
-    </>
+	return (
+		<div>
+			<input type="text" value={searchString} onChange={e => setSearchString(e.target.value)} />
+			<button type="button" onClick={handleClick}>Search</button>
+			{ <ul>
+				{content.map(song => (
+          <li key={song.videoId}>{song.name}</li>
+				))}
+                </ul> }
+		</div>
+	)
 }
 
 export default SearchBar
