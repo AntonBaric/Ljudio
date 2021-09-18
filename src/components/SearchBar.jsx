a36294c9e8a2f220a41736451fe3ced4dc04c913
Anton Baric <anton.baric99@gmail.com>
2021-09-18 23:10:46 +0200

    Axios fetch (not functioning). temporary commit.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function SearchBar() {
 
+    const [songs, setSongs] = useState([])
+
+    useEffect(() => {
+        axios.get('https://yt-music-api.herokuapp.com/api/yt/songs/luvdeluxe')
+        .then(res => {
+            console.log(res);
+            setSongs(res.data)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
     return <>
     <input type="text" />
+    <div>
+        <ul>
+            {songs.map(song => (
+                <li key={song.videoId}>{song.content}</li> )
+                )}
+        </ul>
+    </div>
     </>
 }
 
