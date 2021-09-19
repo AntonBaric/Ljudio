2b0edbd6d1c4fb42f3c8aab5f9c732b1bf8363e2
Anton Baric <anton.baric99@gmail.com>
2021-09-19 15:07:05 +0200

    url search-param functioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
-import SearchBar from './components/SearchBar'
+import NavBar from './components/NavBar'
 import Player from './components/Player'
+import SearchResults from './views/SearchResults'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
 
 function App() {
-  
+
   return (
-    <div className="App">
-      <div className="SearchBar">
-        <SearchBar/>
+    <Router>
+      <div className="App">
+        <div className="SearchBar">
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+            </Route>
+            <Route exact path="/search/:searchString">
+              <SearchResults/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <div className="Player">
-        <Player/>
-      </div>
-    </div>
+    </Router>
   )
 }
 
--- src/views/SearchResults.jsx
+++ src/views/SearchResults.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import NavBar from '../components/NavBar'
+import { useParams } from 'react-router-dom'
 
 function SearchResults() {
-  return (
-    <div>
-      <h1>The Results page</h1>
-    </div>
-  )
+	const [songs, setSongs] = useState([])
+  const {content = [] } = songs // songs must be an array in order to use .map
+  const { searchString } = useParams()
+
+
+	useEffect(() => {
+		axios
+			.get(`https://yt-music-api.herokuapp.com/api/yt/songs/${searchString}`)
+			.then(res => {
+        console.log(res)
+        setSongs(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [searchString])
+
+
+
+	return (
+		<div>
+
+			{ <ul>
+				{content.map(song => (
+          <li key={song.videoId}>{song.name} - {song.artist.name}
+					</li>
+				))}
+                </ul> }
+		</div>
+	)
 }
 
 export default SearchResults
