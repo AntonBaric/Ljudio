b81ab410b93b28495c08740c7e24b2add6b42d60
Anton Baric <anton.baric99@gmail.com>
2021-09-19 21:33:42 +0200

    temp commit

--- src/views/SearchResults.jsx
+++ src/views/SearchResults.jsx
@@ -1,40 +1,58 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
 
 function SearchResults() {
-	const [songs, setSongs] = useState([])
-  const {content = [] } = songs // songs must be an array in order to use .map
+  const [songs, setSongs] = useState([])
+  const [artists, setArtists] = useState([])
+  const { songContent = [] } = songs // songs must be an array in order to use .map
+  const { artistContent = [] } = artists
   const { searchString } = useParams()
 
-
-	useEffect(() => {
-		axios
-			.get(`https://yt-music-api.herokuapp.com/api/yt/songs/${searchString}`)
-			.then(res => {
+  useEffect(() => {
+    axios
+      .get(`https://yt-music-api.herokuapp.com/api/yt/songs/${searchString}`)
+      .then(res => {
         console.log(res)
         setSongs(res.data)
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}, [searchString])
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [searchString])
 
+  useEffect(() => {
+    axios
+      .get(`https://yt-music-api.herokuapp.com/api/yt/artists/${searchString}`)
+      .then(res => {
+        console.log(res)
+        setArtists(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [searchString])
 
-
-	return (
-		<div>
-			{ <ul>
-				{content.map(song => (
+  return (
+    <div>
+      {<ul>
+        {songContent.map(song => (
           <li key={song.videoId}>
             <Link to={`/watch/${song.videoId}`}>
-            {song.name} - {song.artist.name}
+              {song.name} - {song.artist.name}
             </Link>
-					</li>
-				))}
-                </ul> }
-		</div>
-	)
+          </li>
+        ))}
+      </ul>}
+      { <ul>
+        {artistContent.map(artist => (
+          <li key={artist.browseId}>
+            {artist.name}
+          </li>
+        ))}
+      </ul> }
+    </div>
+  )
 }
 
 export default SearchResults
