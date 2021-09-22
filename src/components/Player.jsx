ac8c550273ef340481bcd7da383064ac4f6207c4
Anton Baric <anton.baric99@gmail.com>
2021-09-22 14:18:12 +0200

    play/pause button, volume and seek sliders

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,63 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import ReactPlayer from 'react-player/youtube'
 import { useParams } from 'react-router'
 
 function Player() {
+  const inputRange = useRef(null)
   const { videoId } = useParams('')
+  const [playing, setPlaying] = useState(false)
+  const [seeking, setSeeking] = useState(false)
+  const [volume, SetVolume] = useState(1)
+  const [played, setPlayed] = useState(0)
+
+  const handleSeekMouseDown = e => {
+    setSeeking(true)
+  }
+
+  const handleSeekChange = e => {
+    setPlayed(parseFloat(e.target.value))
+  }
+
+  const handleSeekMouseUp = e => {
+    setSeeking(false)
+    inputRange.current.seekTo(parseFloat(e.target.value))
+  }
+
+  const handleProggress = state => {
+    if (!seeking) {
+      setPlayed(state.played)
+    }
+  }
 
   return <>
-    <ReactPlayer url={'https://www.youtube.com/watch?v=' + videoId}
-      config={{
-        youtube: {
-          playerVars: {
-            showinfo: 1,
-            controls: 1
-          },
-        }
-      }}
-    />
+    <div className="Controls">
+      <ReactPlayer url={'https://www.youtube.com/watch?v=' + videoId}
+        playing={playing}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+        height="0px" width="0px"
+        volume={volume}
+        ref={inputRange}
+        onProgress={handleProggress}
+      />
+      <button onClick={() => setPlaying(playing => !playing)}> {playing ? 'Pause' : 'Play'} </button>
+      <input type="range" min={0} max={1} step="any" value={volume}
+      onChange={e => {SetVolume(e.target.value)}}
+      />
+      <input type='range' min={0} max={1} step='any'
+          value={played}
+          onMouseDown={handleSeekMouseDown}
+          onChange={handleSeekChange}
+          onMouseUp={handleSeekMouseUp}
+        />
+        <p>
+          {played}
+        </p>
+    </div>
     <iframe width="400" height="300"
-    src="https://www.youtube.com/embed/Z8Y1MalRrDc"
-    title="YouTube video player"
-    frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media;
+      src="https://www.youtube.com/embed/Z8Y1MalRrDc"
+      title="YouTube video player"
+      frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media;
     gyroscope; picture-in-picture" allowFullScreen></iframe>
   </>
 }
